78a681393fa9b0a0583f7a4e3fa1fe3bb73c1e4b
David Andika <david.andika@catch.com.au>
2023-03-01 17:33:53 +1100

    Updated nest-commander to latest version and added serviceErrorHandler option

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,12 @@ import { CommandFactory } from 'nest-commander'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule)
+  await CommandFactory.run(AppModule, {
+    logger: false,
+    serviceErrorHandler: (error) => {
+      console.log('My error handler', error)
+    }
+  })
 }
 
 bootstrap()
